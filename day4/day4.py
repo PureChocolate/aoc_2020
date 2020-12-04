d4451dfcc435169d5b3b01453e1a7bf176c5f002
PureChocolate <gurkiratsinghmard@gmail.com>
2020-12-04 13:43:59 -0800

    [Not Working]adding big boy day 4

--- day4/day4.py
+++ day4/day4.py
@@ -1,4 +1,8 @@
+import re
 with open("in4.txt", "r") as f:
+    # for line in f.readlines():
+    #     if line == "\n": bruh += 1
+    # data = [] #f.read().split("\n\n")
     data = []
     s = ""
     for line in f.readlines():
@@ -8,40 +12,66 @@ with open("in4.txt", "r") as f:
         else: s += line + " "
     data.append(s.replace("\n","")[:-1])
 
+# for i in range(10):
+#     print data[i]
+# print len(data)
+
 rules = ["byr", "iyr","eyr","hgt","hcl","ecl","pid","cid"]
 clrs = ["amb","blu","brn","gry","grn","hzl","oth"]
 c = 0
-def dataValid(temp):
-    for i in range(len(temp)):
-        temp[i] = temp[i].split(":")
-        val = temp[i][1]
-        par = temp[i][0]
-        if par == "ecl" and not val in clrs: 
+def dataValid(pars,vals):
+    for i in range(len(pars)):
+        val = vals[i]
+        par = pars[i]
+        if par == "ecl" and not val in clrs:
             return False
-        if par == "hcl" and not val[:1] == "#": 
-            if not val[1:].isalnum() or not len(val[1:]) == 6:
+        if par == "hcl" and val[:1] == "#":
+            if all( c in set("abcdef0123456789") for c in val[1:]) and len(val[1:]) == 6:
+                return True
+            else:
                 return False
-        if par == "pid" and not len(val) == 9:
-            return False
-        if par == "hgt":
-            if val[-2:] == "in" and not 76 >= int(val[:-2]) >= 59:
+        if par == "pid":
+            if val.isdigit(): 
+                if len(val) == 9: 
+                    return True
+            return False            
+        try:
+            if par == "hgt":
+                if val[-2:] == "in" and not 76 >= int(val[:-2]) >= 59:
+                    return False
+                elif val[-2:] == "cm" and not 193 >= int(val[:-2]) >= 150:
+                    return False
+                elif not val[-2:].isalpha():
+                    return False
+            if par == "byr" and not 2002 >= int(val) >= 1920:
                 return False
-            elif val[-2:] == "cm" and not 193 >= int(val[:-2]) >= 150:
+            if par == "iyr" and not 2020 >= int(val) >= 2010:
                 return False
-            elif not val[-2:].isalpha():
+            if par == "eyr" and not 2030 >= int(val) >= 2020:
                 return False
-        if par == "byr" and not 2002 >= int(val) >= 1920:
-            return False
-        if par == "iyr" and not 2020 >= int(val) >= 2010:
-            return False
-        if par == "eyr" and not 2030 >= int(val) >= 2020:
+        except ValueError:
             return False
     return True
 
 for i in range(len(data)):
-    if all(r in data[i] for r in rules[:7]):
-        test = [ a for a in data[i].split(" ")]
-        if dataValid(test): c += 1 
+    # print "\n"
+    test = [s for s in data[i].split()]
+    pars = ""
+    vals = ""
+    for k in range(len(test)):
+        pars += test[k].split(":")[0] + " "
+        vals += test[k].split(":")[1] + " "
+    pars = pars.split()
+    vals = vals.split()
+    # if(set(pars) == set(rules[:-1])): print "poggers"
+    if all(w in pars for w in rules[:7]):
+        # print str(pars) + "///////////////"
+        # print str(vals) + "$$$$$$$$$$$$$$$"
+        if dataValid(pars,vals): c += 1
+    # if all(r in data[i] for r in rules[:7]):
+        # c += 1
+        # test = [ a for a in data[i].split(" ")]
+        # if dataValid(test): c += 1 
 
 print c
 
