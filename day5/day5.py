0669f4efd7d1c325dc5d02ebf169625ef13d3178
PureChocolate <gurkiratsinghmard@gmail.com>
2020-12-05 10:17:22 -0800

    Improved day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,46 +1,12 @@
-import math as m
-with open("in5.txt", "r") as f: 
-    bdata = f.read().split("\n")
-    
-row = 0
-col = 0
-seatID = 0
+with open("in5.txt", "r") as f: bp = f.read().split("\n")
+max = 0
 seats = []
-for i in range(len(bdata)):
-    maxR = 127
-    minR = 0
-    maxC = 7
-    minC = 0
-    for a in range(len(bdata[i])):
-        if bdata[i][a] == "F":
-            if a == 6:
-                row = minR
-                continue
-            maxR = m.floor((maxR + minR)/2.0)
-            minR = minR
-        elif bdata[i][a] == "B":
-            if a == 6:
-                row = maxR
-                continue
-            maxR = maxR
-            minR = m.ceil((minR + maxR)/2.0)
-        elif bdata[i][a] == "R":
-            if a == len(bdata[i])-1:
-                col = maxC
-                continue
-            maxC = maxC
-            minC = m.ceil((minC + maxC)/2.0)        
-        elif bdata[i][a] == "L":
-            if a == len(bdata[i])-1:
-                col = minC
-                continue
-            maxC = m.floor((maxC + minC)/2.0)
-            minC = minC
-    seats.append(((row * 8) + col))
-    if ((row * 8) + col) > seatID : seatID = ((row * 8) + col)
-
-for l in range(len(seats)):
-    if l not in seats and  900 > l > 100:
-        print l
-
-print seatID
+for a in bp:
+    num = (a.translate(str.maketrans({"B":"1", "F": "0", "R":"1","L":"0"})))
+    val = int(num[-3:],2)
+    num = int(num[:7],2)
+    seats.append((num * 8) + val)
+    if (num * 8) + val > max: max = (num * 8) + val
+for i in range(len(bp)):
+    if i + 1 in seats and i - 1 in seats and i not in seats: print(i)
+print(max)
